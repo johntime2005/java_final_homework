566b7938db9f192fd4d774b7289bf86de79deb1c
lightshineforyou <3401517035@qq.com>
2024-12-19 21:30:24 +0800

    update readme and add 说明文档

--- LibraryAccount.java
+++ LibraryAccount.java
@@ -1,22 +1,26 @@
 /*
-银行内用户Id：10位数字组成
+图书馆内用户Id：10位数字组成
 用户名：填姓名（10个汉字长度）
 密码：不少于4位
 身份ID：填学号（12位数字）
 手机号：正常手机号
 性别：用一位字符（M:男，F:女）
+专业：十位以内
+班级：int类型即可
 出生日期：YYYY-MM-DD
 账户余额：双精度范围即可
  */
 public class LibraryAccount {
     private String accountid;
     private String name;
-    private String password;
-    private String identifyid;
+    private String password;//密码
+    private String identifyid;//身份ID
     private String phonenumber;
     private char gender;
     private String birthday;
-    private double balance;
+    private int balance;
+    private String major;
+    private int class;
     public LibraryAccount(String accountid,String name,String password,String identifyid,
                        String phonenumber,char gender,String birthday)
     {
@@ -27,9 +31,9 @@ public class LibraryAccount {
         this.phonenumber = phonenumber;
         this.gender=gender;
         this.birthday=birthday;
-        this.balance=2000;
+        this.balance=10;//
     }
-    //getter
+    //Getters
     public String getAccountId() {
         return accountId;
     }
@@ -61,7 +65,12 @@ public class LibraryAccount {
     public double getBalance() {
         return balance;
     }
-
+    public String getMajor(){
+        return major;
+    }
+    public int getClass(){
+        return class;
+    }
     // Setters
     public void setAccountId(String accountId) {
         this.accountId = accountId;
@@ -93,6 +102,13 @@ public class LibraryAccount {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+    public void setMajor(String major)
+    {
+        this.major = major;
+    }
+    public void setClass(int class){
+        this.class = class;
     }
     @Override
     public String toString()
