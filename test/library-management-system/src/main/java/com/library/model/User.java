7c0b6e8b45b43c3691b3e149c2ad74588c2580a0
johntime <johntimeson@outlook.com>
2024-12-26 16:36:00 +0800

    新建基于javafx的项目于test文件夹中

---
+++ test/library-management-system/src/main/java/com/library/model/User.java
@@ -0,0 +1,34 @@
+package com.library.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<Book> borrowedBooks;
+
+    public User(String username) {
+        this.username = username;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+}
